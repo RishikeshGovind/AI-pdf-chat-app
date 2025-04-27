ddb452368356b71bd9668f57d9b851ed5d5b74a9
Rishi Govind <rishigovind@Rishis-Air.broadband>
2025-04-28 00:11:00 +0100

    text2text flanbase

--- app.py
+++ app.py
@@ -46,10 +46,12 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     llm = HuggingFaceHub(
-        repo_id="google/flan-t5-base",
-        huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"), 
-        model_kwargs={"temperature": 0.5, "max_length": 512}
+    repo_id="google/flan-t5-base",
+    task="text2text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+    model_kwargs={"temperature": 0.5, "max_length": 512}
     )
+
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True
